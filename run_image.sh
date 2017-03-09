e67749551bf9077c2e78bc18f73180cfb9f2495b
pete <whoget@gmail.com>
2017-03-09 20:01:30 +0700

    update run_image and set port to 5222

--- run_image.sh
+++ run_image.sh
@@ -1,1 +1,1 @@
-docker run -d -p 9080:80 -v /opt/mapserver/map:/map -v /opt/mapserver/var/www:/var/www  lat_mapserver
+docker run -d --name lat_mapserver_1  -p 5222:80 -v /opt/mapserver/map:/map -v /opt/mapserver/var/www:/var/www  lat_mapserver
